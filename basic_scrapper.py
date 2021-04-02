7ce17469be9c8e38ce4c9e19584c12c34f614080
mandahadi <masoema@gmail.com>
2021-04-02 18:58:42 +0700

    simple crawl to url_list.txt save crawl result to .csv test crawl url data, date, lang, user_agent, status, content_type

--- basic_scrapper.py
+++ basic_scrapper.py
@@ -1,11 +1,12 @@
 import urllib.request
 from bs4 import BeautifulSoup
-from selenium import webdriver
+from seleniumwire import webdriver
 from selenium.webdriver.chrome.options import Options
 from random_user_agent.user_agent import UserAgent
 from random_user_agent.params import SoftwareName, OperatingSystem
 from selenium.common.exceptions import ElementClickInterceptedException, ElementNotInteractableException
 import time
+import pandas as pd
 
 
 def browser_options_setup():
@@ -26,7 +27,8 @@ def browser_options_setup():
     options.add_argument("--incognito")
     options.add_argument('--allow-running-insecure-content')
     options.add_argument("--lang=en-GB")
-    # options.add_argument("--headless")
+    options.add_argument('log-level=3')
+    options.add_argument("--headless")
 
     options.set_capability("acceptInsecureCerts", True)
     return options, user_agent, headers
@@ -37,9 +39,45 @@ def spawn_browser(options, user_agent, headers):
     browser.header['User-Agent'] = user_agent
     return browser
 
+def save_request_details(result):
+    df = pd.DataFrame(result)
+    #df.to_excel("C:\Users\mandah-acer\Documents\webcrawler\crawling_result.xlsx")
+    df.to_csv("C:\Users\mandah-acer\Documents\webcrawler\crawling_result.csv")
+
+def get_requests_details(browser, result):
+    count = 0
+    for request in browser.requests:
+        if request.response:
+            request_url = request.url
+            request_date = request.date
+            request_language = request.headers['Accept-Language']
+            request_user_agent = request.headers['User-Agent']        
+            response_date = request.response.date
+            response_status = request.response.status_code
+            response_content_type = request.response.headers['Content-Type']
+            count += 1
+            result.append({"url": browser.current_url, "webpage_title": browser.title, "request_url": request_url, "request_date": request_date, "request_language": request_language, "request_user_agent": request_user_agent, "response_date": response_date, "response_status": response_status, "response_content_type": response_content_type})
+            save_request_details(result)
+            # print(request_url, request_date, request_language, request_user_agent, response_date, response_status, response_content_type)
+        if count == 10: break # to limit only to get 10 earliest requests, comment to disable
+
+result = []
 start = time.time()
-url = 'https://amazon.com'
 options, user_agent, headers = browser_options_setup()
 browser = spawn_browser(options, user_agent, headers)
-browser.get(url)
-browser.implicitly_wait(5)
+
+url_file = open("url_list.txt", "r")
+url_list = url_file.read().replace("\n",",").split(",")
+url_list = [url for url in url_list if url != ""]
+print("URL List:", url_list)
+
+for url in url_list:
+    print("Processing:", url)
+    try:
+        browser.get(url)
+        browser.implicitly_wait(5)
+        get_requests_details(browser, result)
+    except Exception:
+        continue
+    print("Current results count:", len(result))
+    print("Elapsed Time: {}(s)".format(round(time.time()-start,2)))
